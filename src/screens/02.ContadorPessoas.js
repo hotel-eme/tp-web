9ff01e4c4539b5a044de0dc9d822e901a87d6101
Evandro Myller <evandro.vieira1@aluno.unip.br>
2021-10-30 19:48:19 -0300

    Implementa base da tela "Contador de Pessoas"

---
+++ src/screens/02.ContadorPessoas.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export function ContadorPessoasScreen() {
+  return (
+    <View>
+      <Text>Contador de pessoas</Text>
+    </View>
+  );
+}
