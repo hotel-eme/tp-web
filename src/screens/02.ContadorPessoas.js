bb4848fcd631a8eb38244d7b8858d3c24552ce54
Evandro Myller <evandro.vieira1@aluno.unip.br>
2021-10-30 20:35:24 -0300

    Soma e subtrai pessoas no contador

--- src/screens/02.ContadorPessoas.js
+++ src/screens/02.ContadorPessoas.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Text, View } from 'react-native';
 
 export function ContadorPessoasScreen() {
+  var [contador, setContador] = useState(0);
+
+  function adicionaPessoa() {
+    setContador(contador + 1);
+  }
+
+  function removePessoa() {
+    setContador(contador - 1);
+  }
+
   return (
     <View>
-      <Text style={{ fontSize: 40 }}>0</Text>
-      <Button title="+"></Button>
-      <Button title="-"></Button>
+      <Text style={{ fontSize: 40 }}>{contador}</Text>
+      <Button title="+" onPress={adicionaPessoa}></Button>
+      <Button title="-" onPress={removePessoa}></Button>
     </View>
   );
 }
