b034c828cede2d7ae6f1973e081761e148af4693
Alienne Silva <aliennecarlos@gmail.com>
2021-10-31 10:45:05 -0300

    Inclui multiplicador de número no menu

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from 'react';
 import { Button, View } from 'react-native';
 import { MeuPerfilScreen } from "./src/screens/01.MeuPerfil";
+import { MultiplicadorDoisNumerosScreen } from "./src/screens/03.MultiplicadorDoisNumeros";
 
 const Stack = createNativeStackNavigator();
 
@@ -10,6 +11,7 @@ function HomeScreen(props) {
   return (
     <View>
       <Button title="Meu Perfil" onPress={function () { props.navigation.navigate('Meu Perfil') }} />
+      <Button title="Multiplicador de Dois Números" onPress={function () { props.navigation.navigate('Multiplicador de Dois Números') }} />
     </View>
   );
 }
@@ -20,6 +22,7 @@ export default function App() {
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Meu Perfil" component={MeuPerfilScreen} />
+        <Stack.Screen name="Multiplicador de Dois Números" component={MultiplicadorDoisNumerosScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
