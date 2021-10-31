4f05665b76bd8239dc2ff6b9eb19653ed21d75ac
Alienne Silva <aliennecarlos@gmail.com>
2021-10-31 12:31:00 -0300

    Corrige a indentação

--- src/screens/03.MultiplicadorDoisNumeros.js
+++ src/screens/03.MultiplicadorDoisNumeros.js
@@ -2,30 +2,20 @@ import React, {useState} from 'react';
 import { Button, Text, View, TextInput } from 'react-native';
 
 export function MultiplicadorDoisNumerosScreen() {
+  var [number1, setNumber1] = useState(null);
+  var [number2, setNumber2] = useState(null);
+  var [resultado, setResultado] = useState(null);
 
-    var [number1, setNumber1] = useState(null);
-    var [number2, setNumber2] = useState(null);
-    var [resultado, setResultado] = useState(null);
-    function calcula(){
-      setResultado(number1 * number2);
-    }
-    return (
-    <View style={{textAlign: 'center' }}>
-      <Text style ={{fontSize:35, textAlign: 'center' }}>Multiplicador de Dois Números</Text>
-      <TextInput
-      style = {{fontSize:20, textAlign: 'left' }}
-      placeholder= "Digite um numero:"
-      onChangeText={setNumber1}
-      />
-      <TextInput
-      style = {{fontSize:20, textAlign: 'left' }}
-      placeholder= "Digite um numero:"
-      onChangeText={setNumber2}
-     />
+  function calcula(){
+    setResultado(number1 * number2);
+  }
+
+  return (
+    <View>
+      <TextInput placeholder="Digite um numero:" onChangeText={setNumber1}/>
+      <TextInput placeholder="Digite um numero:" onChangeText={setNumber2}/>
       <Button title="Calcular" onPress={calcula}></Button>
-      <Text>Resultado:{resultado}</Text>
-
+      <Text>Resultado: {resultado}</Text>
     </View>
-
   );
 }
