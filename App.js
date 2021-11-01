1cc0b34273eac86de0aafcc14d2b623ed52277af
Carolina Model <carolinamodeldw@gmail.com>
2021-10-31 22:56:59 -0300

    Adiciona tela "Meu Perfil Drawer" na tela incial

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { Button, View } from 'react-native';
 import { MeuPerfilScreen } from "./src/screens/01.MeuPerfil";
 import { ContadorPessoasScreen } from "./src/screens/02.ContadorPessoas";
 import { MultiplicadorDoisNumerosScreen } from "./src/screens/03.MultiplicadorDoisNumeros";
+import { MeuPerfilScreenDrawer } from "./src/screens/12.MeuPerfilDrawer";
 
 const Stack = createNativeStackNavigator();
 
@@ -14,6 +15,8 @@ function HomeScreen(props) {
       <Button title="Meu Perfil" onPress={function () { props.navigation.navigate('Meu Perfil') }} />
       <Button title="Contador de Pessoas" onPress={function () { props.navigation.navigate('Contador de Pessoas') }} />
       <Button title="Multiplicador de Dois Números" onPress={function () { props.navigation.navigate('Multiplicador de Dois Números') }} />
+      <Button title="Meu Perfil com Drawer" onPress={function () { props.navigation.navigate('Meu Perfil com Drawer') }} />
+
     </View>
   );
 }
@@ -26,6 +29,7 @@ export default function App() {
         <Stack.Screen name="Meu Perfil" component={MeuPerfilScreen} />
         <Stack.Screen name="Contador de Pessoas" component={ContadorPessoasScreen} />
         <Stack.Screen name="Multiplicador de Dois Números" component={MultiplicadorDoisNumerosScreen} />
+        <Stack.Screen name="Meu Perfil com Drawer" component={MeuPerfilScreenDrawer} />
       </Stack.Navigator>
     </NavigationContainer>
   );
