f8a4c00ad613c3cbbb3f72f7b536bbbcf7b42c5a
Alienne Silva <aliennecarlos@gmail.com>
2021-11-01 08:51:13 -0300

    Corrige espaços no código.

--- src/screens/08.AnuncioParaVendaProdutos.js
+++ src/screens/08.AnuncioParaVendaProdutos.js
@@ -5,7 +5,7 @@ export function AnuncioParaVendaProdutosScreen() {
   return (
     <View>
       <Text>RSA Produtos Mineiros</Text>
-      <ScrollView horizontal = {true} showHorizontalScrollIndicator={true}>
+      <ScrollView horizontal={true} showHorizontalScrollIndicator={true}>
         <Image style={{width: 500, height: 500}} source={ require('../imagens/doces.png') } />
         <Image style={{width: 500, height: 500}} source={ require('../imagens/queijofrecal.png') } />
         <Image style={{width: 500, height: 500}} source={ require('../imagens/queijogouda.png') } />
