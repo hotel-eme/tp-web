aa276c1d4041f1568dc4995327cf1dbdc8e08549
Mauricio Schaefer <contato.schaefer.m@gmail.com>
2021-11-02 17:59:04 -0300

    Aprimora codigo com props

--- src/screens/09.VagaEmprego.js
+++ src/screens/09.VagaEmprego.js
@@ -1,37 +1,55 @@
 import React from 'react';
 import { Image, Text, ScrollView, View } from 'react-native';
 
+function VagaEmprego(props) {
+  return (
+    <View style={{ marginBottom: 10 }}>
+      <Text style={{ fontWeight: 'bold' }}>{props.nome}</Text>
+      <Text>Salário: {props.salario}</Text>
+      <Text>Beneficios: {props.beneficio}</Text>
+      <Text>Descrição: {props.descricao}</Text>
+      <Text>Contato: {props.contato}</Text>
+    </View>
+  );
+}
 
 export function VagaEmpregoScreen() {
   return (
     <View>
       <ScrollView vertical={true} showsVerticalScrollIndicator={true}>
-        <Text>Desenvolvedor Backend mobile</Text>
-        <Text>Salário: R$2.500</Text>
-        <Text>Beneficios: Vale Alimentação, Vale Condução, Plano Odontológico.</Text>
-        <Text>Descrição: Desenvolver backend com foco em hotelaria, necessário conhecimento ReactJS, Python.</Text>
-        <Text>Contato: 13 999999990 - Ramal 013</Text>
-        {'\n'}
-        <Text>Estágio de programação</Text>
-        <Text>Salário: não informado</Text>
-        <Text>Beneficios: Vale Alimentação, Vale Transporte, Plano de saúde, Auxilio HomeOffice</Text>
-        <Text>Descrição: Iniciar estágio para área de programação, será disponibilizado todo material necessário para trabalho
-        assim como equipamentos se necessário, necessário apenas ensino médio completo.</Text>
-        <Text>Contato: 13 999999991 - Ramal 130</Text>
-        {'\n'}
-        <Text>Pesquisador na área de Física nuclear</Text>
-        <Text>Salário: a definir</Text>
-        <Text>Beneficios: Plano odontológico, Bonificação anual de acordo com desempenho</Text>
-        <Text>Descrição: Vagas para realizar pesquisar no campo de física nuclear, necessário formação em física e pós graduação em física nuclear</Text>
-        <Text>Observação: Em caso de contratação, o funcionário deverá se mudar para Hill Valley - Califórnia, uma instalação completa para pesquisas completo
-        laboratórios, centro hospitalar, refeitório para todos os funcionários.</Text>
-        <Text>Contato: 5-4385</Text>
-        {'\n'}
-        <Text>Programador Frontend</Text>
-        <Text>Salário: a definir</Text>
-        <Text>Beneficios: Vale Alimentação, Vale Transporte, Plano de saúde, Plano odontológico</Text>
-        <Text>Descrição: Vaga para programdor frontend com experiencia em HTML, CSS, JavaScript, Full Stack</Text>
-        <Text>Contato: 13 999999992</Text>
+        <VagaEmprego
+          nome='Desenvolvedor Backend mobile'
+          salario={2500}
+          beneficio='VR, VA, Vale Condução, Plano Odontológico'
+          descricao='Desenvolver backend com foco em hotelaria, necessário conhecimento ReactJS, Python'
+          contato='13 999999990 - ramal 013'
+        />
+
+        <VagaEmprego
+          nome='Estágio de programação'
+          salario='não informado'
+          beneficio='VR, VA, VT, Plano de saúde'
+          descricao='Iniciar estágio para área de programação, será disponibilizado todo material necessário para trabalho
+            assim como equipamentos se necessário, necessário apenas ensino médio completo.'
+          contato='13 999999991 - ramal 130'
+        />
+
+        <VagaEmprego
+          nome='Pesquisador na área de Física nuclear'
+          salario='a definir'
+          beneficio='Plano odontológico, Bonificação anual de acordo com desempenho'
+          descricao='Vagas para realizar pesquisar no campo de física nuclear, necessário formação em física e
+            pós graduação em física nuclear em Hill Valley - Califórnia'
+          contato='5-4385'
+        />
+
+        <VagaEmprego
+          nome='Programador Frontend'
+          salario={2800}
+          beneficio='VR, VA ,VT, Plano de saúde'
+          descricao='Vaga para programdor frontend com experiencia em HTML, CSS, JavaScript, Full Stack'
+          contato='13 999999992'
+        />
       </ScrollView>
     </View>
   );
