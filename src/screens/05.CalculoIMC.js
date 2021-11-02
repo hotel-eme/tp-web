13331d665271630e54cadcd30d36cbada348c670
Alienne Silva <aliennecarlos@gmail.com>
2021-11-02 11:39:37 -0300

    Corrige a indentação e as inequações

--- src/screens/05.CalculoIMC.js
+++ src/screens/05.CalculoIMC.js
@@ -8,21 +8,32 @@ export function CalculoIMCScreen() {
   var [classificacao, setClassificacao] = useState(null);
 
   function verifica() {
-    var imc = peso / (altura * altura)
+    var imc=peso/(altura * altura)
     setResultado(imc);
-    if (imc < 18.5){
+    if (imc <= 18.5){
       setClassificacao ("Abaixo do Peso")
-    }else if ((imc > 18.6) && (imc < 24.9)){
+    }
+    else
+    if ((imc >= 18.6)&&(imc <= 24.9)){
       setClassificacao ("Peso Normal")
-    }else if ((imc > 25) && (imc < 29.9)){
+    }
+    else
+    if ((imc > 25)&&(imc < 29.9)){
       setClassificacao ("Sobrepeso")
-    }else if ((imc > 30) && (imc < 34.9)){
+    }
+    else
+    if ((imc > 30)&&(imc < 34.9)){
       setClassificacao ("Obesidade Grau I")
-    }else if ((imc > 35) && (imc < 39.9)){
+    }
+    else
+    if ((imc > 35)&&(imc < 39.9)){
       setClassificacao ("Obesidade Grau II")
-    }else if (imc >= 40){
+    }
+    else
+    if (imc >= 40){
       setClassificacao ("Obesidade Grau III ou Móbida")
-    }else {
+    }
+    else{
       setClassificacao ("Verifique os valores digitados")
     }
   }
