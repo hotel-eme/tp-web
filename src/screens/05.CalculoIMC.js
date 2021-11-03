19a6c10a84e97cc61c7152440dfc19437533c548
Evandro Myller <evandro.vieira1@aluno.unip.br>
2021-11-02 22:36:47 -0300

    Ajusta sintaxe de código para padrões

--- src/screens/05.CalculoIMC.js
+++ src/screens/05.CalculoIMC.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, Text, TextInput, View, Image } from 'react-native';
+import { Button, Image, Text, TextInput, View } from 'react-native';
 
 export function CalculoIMCScreen() {
   var [peso, setPeso] = useState(null);
@@ -8,38 +8,33 @@ export function CalculoIMCScreen() {
   var [classificacao, setClassificacao] = useState(null);
 
   function verifica() {
-    var imc=peso/(altura * altura)
+    var imc = peso / (altura * altura);
     setResultado(imc);
-    if (imc <= 18.5){
-      setClassificacao ("Abaixo do Peso")
+    if (imc <= 18.5) {
+      setClassificacao("Abaixo do Peso");
     }
-    else
-    if ((imc >= 18.6)&&(imc <= 24.9)){
-      setClassificacao ("Peso Normal")
+    else if ((imc >= 18.6) && (imc <= 24.9)) {
+      setClassificacao("Peso Normal");
     }
-    else
-    if ((imc > 25)&&(imc < 29.9)){
-      setClassificacao ("Sobrepeso")
+    else if ((imc > 25) && (imc < 29.9)) {
+      setClassificacao("Sobrepeso");
     }
-    else
-    if ((imc > 30)&&(imc < 34.9)){
-      setClassificacao ("Obesidade Grau I")
+    else if ((imc > 30) && (imc < 34.9)) {
+      setClassificacao("Obesidade Grau I");
     }
-    else
-    if ((imc > 35)&&(imc < 39.9)){
-      setClassificacao ("Obesidade Grau II")
+    else if ((imc > 35) && (imc < 39.9)) {
+      setClassificacao("Obesidade Grau II");
     }
-    else
-    if (imc >= 40){
-      setClassificacao ("Obesidade Grau III ou Móbida")
+    else if (imc >= 40) {
+      setClassificacao("Obesidade Grau III ou Móbida");
     }
-    else{
-      setClassificacao ("Verifique os valores digitados")
+    else {
+      setClassificacao("Verifique os valores digitados");
     }
   }
   return (
     <View>
-      <Image style={{width: 200, height: 200}} source={ require('../imagens/IMC.png')} />
+      <Image style={{ width: 200, height: 200 }} source={require('../imagens/IMC.png')} />
       <TextInput placeholder="Digite sua altura, em m:" onChangeText={setAltura} />
       <TextInput placeholder="Digite o seu peso, em kg:" onChangeText={setPeso} />
       <Button title="Verificar" onPress={verifica} />
