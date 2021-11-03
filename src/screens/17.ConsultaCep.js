e394b19dd2b506ab0ab78f2f1ca7e5f407a3ccd4
Carolina Model <carolinamodeldw@gmail.com>
2021-11-02 22:21:03 -0300

    Atualiza tela de consulta de Cep

--- src/screens/17.ConsultaCep.js
+++ src/screens/17.ConsultaCep.js
@@ -1,20 +1,42 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import { TextInput, View, Text, Button } from 'react-native';
 
 export function MeuCepScreen() {
   var [cep, setCep] = useState(null);
-  var [resultado, setResultado] = useState(null);
+  var [logradouro, setLogradouro] = useState(null);
+  var [bairro, setBairro] = useState(null);
+  var [complemento, setComplemento] = useState(null);
+  var [localidade, setlocalidade] = useState(null);
+  var [uf, setUf] = useState(null);
+  var [ddd, setDdd] = useState(null);
+
+  function trataResposta(response) {
+    setDdd(response.data.ddd);
+    setUf(response.data.uf);
+    setlocalidade(response.data.localidade);
+    setComplemento(response.data.complemento);
+    setBairro(response.data.bairro);
+    setLogradouro(response.data.logradouro);
+  }
 
   function buscaCep() {
-    setResultado(cep);
+    axios({
+      method: 'get',
+      url: `https://viacep.com.br/ws/${cep}/json/`,
+    }).then(trataResposta);
   }
 
   return (
     <View>
       <TextInput placeholder="Digite um CEP, sem pontos:" onChangeText={setCep} />
       <Button title="Exibir" onPress={buscaCep} />
-      <Text>Resultado: {resultado}</Text>
-      <Text>cep: {cep}</Text>
+      <Text>Logradouro: {logradouro}</Text>
+      <Text>Bairro: {bairro}</Text>
+      <Text>Complemento: {complemento}</Text>
+      <Text>Localidade: {localidade}</Text>
+      <Text>UF: {uf}</Text>
+      <Text>DDD: {ddd}</Text>
     </View>
   );
 }
