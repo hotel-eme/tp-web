8557b0d46d1c28d81d47622f0270a97d30c4d55a
Alienne Silva <aliennecarlos@gmail.com>
2021-11-03 08:26:59 -0300

    Inclui arquivo filmes

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { Button, View } from 'react-native';
 import { MeuPerfilScreen } from "./src/screens/01.MeuPerfil";
 import { ContadorPessoasScreen } from "./src/screens/02.ContadorPessoas";
 import { MultiplicadorDoisNumerosScreen } from "./src/screens/03.MultiplicadorDoisNumeros";
+import { filmesScreen } from "./src/screens/20.filmes";
 
 const Stack = createNativeStackNavigator();
 
@@ -14,6 +15,7 @@ function HomeScreen(props) {
       <Button title="Meu Perfil" onPress={function () { props.navigation.navigate('Meu Perfil') }} />
       <Button title="Contador de Pessoas" onPress={function () { props.navigation.navigate('Contador de Pessoas') }} />
       <Button title="Multiplicador de Dois Números" onPress={function () { props.navigation.navigate('Multiplicador de Dois Números') }} />
+      <Button title="Filmes" onPress={function () { props.navigation.navigate('Filmes') }} />
     </View>
   );
 }
@@ -26,6 +28,7 @@ export default function App() {
         <Stack.Screen name="Meu Perfil" component={MeuPerfilScreen} />
         <Stack.Screen name="Contador de Pessoas" component={ContadorPessoasScreen} />
         <Stack.Screen name="Multiplicador de Dois Números" component={MultiplicadorDoisNumerosScreen} />
+        <Stack.Screen name="Filmes" component={filmesScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
