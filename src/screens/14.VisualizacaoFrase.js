2675e5928e359d0d2a0ffa6debd1d114c583df09
Evandro Myller <evandro.vieira1@aluno.unip.br>
2021-11-04 13:59:59 -0300

    Salva configurações na memória do dispositivo

--- src/screens/14.VisualizacaoFrase.js
+++ src/screens/14.VisualizacaoFrase.js
@@ -1,3 +1,4 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import React, { useState } from "react";
 import { Switch, Text, View } from "react-native";
 
@@ -12,13 +13,25 @@ export function VisualizacaoFraseScreen() {
     fontSize: fonteGrande ? 25 : 15,
   };
 
+  // Carrega configurações e altera estados
+  (async function () {
+    setModoNoturno(JSON.parse(await AsyncStorage.getItem('modoNoturno')));
+    setFonteGrande(JSON.parse(await AsyncStorage.getItem('fonteGrande')));
+  })();
+
   return (
     <View>
       <View >
         <Text>Modo Noturno</Text>
-        <Switch value={modoNoturno} onValueChange={setModoNoturno} />
+        <Switch value={modoNoturno} onValueChange={function (valor) {
+          AsyncStorage.setItem('modoNoturno', JSON.stringify(valor));
+          setModoNoturno(valor);
+        }} />
         <Text>Tamanho da Fonte</Text>
-        <Switch value={fonteGrande} onValueChange={setFonteGrande} />
+        <Switch value={fonteGrande} onValueChange={function (valor) {
+          AsyncStorage.setItem('fonteGrande', JSON.stringify(valor));
+          setFonteGrande(valor);
+        }} />
       </View>
       <View>
         <Text style={style}>
