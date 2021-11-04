aa981273470a2bf614e66bdd5a3a6322139566ef
Evandro Myller <evandro.vieira1@aluno.unip.br>
2021-11-04 17:59:33 -0300

    Grava estados do componente se conversão de moeda

--- src/screens/10.ConversorMoedas.js
+++ src/screens/10.ConversorMoedas.js
@@ -1,22 +1,26 @@
 import { Picker } from '@react-native-picker/picker';
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Text, TextInput, View } from 'react-native';
 
 export function ConversorMoedasScreen() {
+  var [valor, setValor] = useState(null);
+  var [moedaOrigem, setMoedaOrigem] = useState(null);
+  var [moedaDestino, setMoedaDestino] = useState(null);
+
   return (
     <View>
       <Text>Conversor de moedas: Dolar, Real e Euro</Text>
       <Text>Valor:</Text>
-      <TextInput />
+      <TextInput onChangeText={setValor} />
       <Text>De:</Text>
-      <Picker>
+      <Picker onValueChange={setMoedaOrigem}>
         <Picker.Item label="Selecione" />
         <Picker.Item value="USD" label="Dólar" />
         <Picker.Item value="EUR" label="Euro" />
         <Picker.Item value="BRL" label="Real" />
       </Picker>
       <Text>Para:</Text>
-      <Picker>
+      <Picker onValueChange={setMoedaDestino}>
         <Picker.Item label="Selecione" />
         <Picker.Item value="USD" label="Dólar" />
         <Picker.Item value="EUR" label="Euro" />
