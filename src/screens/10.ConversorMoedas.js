033361efb242df006a4bbbe288fec664de445a3f
Evandro Myller <evandro.vieira1@aluno.unip.br>
2021-11-04 17:45:17 -0300

    Insere seleção de moedas

--- src/screens/10.ConversorMoedas.js
+++ src/screens/10.ConversorMoedas.js
@@ -1,13 +1,27 @@
+import { Picker } from '@react-native-picker/picker';
 import React from 'react';
-import { Button, Text, View } from 'react-native';
+import { Button, Text, TextInput, View } from 'react-native';
 
 export function ConversorMoedasScreen() {
   return (
     <View>
       <Text>Conversor de moedas: Dolar, Real e Euro</Text>
-      <Text>Valor: </Text>
-      <Text>De: </Text>
-      <Text>Para: </Text>
+      <Text>Valor:</Text>
+      <TextInput />
+      <Text>De:</Text>
+      <Picker>
+        <Picker.Item label="Selecione" />
+        <Picker.Item value="USD" label="Dólar" />
+        <Picker.Item value="EUR" label="Euro" />
+        <Picker.Item value="BRL" label="Real" />
+      </Picker>
+      <Text>Para:</Text>
+      <Picker>
+        <Picker.Item label="Selecione" />
+        <Picker.Item value="USD" label="Dólar" />
+        <Picker.Item value="EUR" label="Euro" />
+        <Picker.Item value="BRL" label="Real" />
+      </Picker>
       <Button title="Converter"></Button>
     </View>
   );
