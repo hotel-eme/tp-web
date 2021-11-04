b81637c6733b5980dfd700291265386036abdf83
Evandro Myller <evandro.vieira1@aluno.unip.br>
2021-11-04 18:47:14 -0300

    Implementa conversão de moedas

--- src/screens/10.ConversorMoedas.js
+++ src/screens/10.ConversorMoedas.js
@@ -6,6 +6,18 @@ export function ConversorMoedasScreen() {
   var [valor, setValor] = useState(null);
   var [moedaOrigem, setMoedaOrigem] = useState(null);
   var [moedaDestino, setMoedaDestino] = useState(null);
+  var [resultado, setResultado] = useState(null);
+
+  function converterMoeda() {
+    const tabelaConversao = {
+      USD: 1,  // referência
+      BRL: 5.55,
+      EUR: 0.86,
+    };
+    const valorEmUSD = valor / tabelaConversao[moedaOrigem];
+    const valorConvertido = valorEmUSD * tabelaConversao[moedaDestino];
+    setResultado(valorConvertido);
+  }
 
   return (
     <View>
@@ -26,7 +38,8 @@ export function ConversorMoedasScreen() {
         <Picker.Item value="EUR" label="Euro" />
         <Picker.Item value="BRL" label="Real" />
       </Picker>
-      <Button title="Converter"></Button>
+      <Button title="Converter" onPress={converterMoeda}></Button>
+      {resultado && <Text>Resultado: {moedaDestino} {resultado.toFixed(2)}</Text>}
     </View>
   );
 }
