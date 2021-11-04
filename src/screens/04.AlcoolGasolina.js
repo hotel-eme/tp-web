dc4daeee05551a7ac48b3ae2032d031ddca77dde
Mauricio Schaefer <contato.schaefer.m@gmail.com>
2021-11-04 14:14:47 -0300

    Adiciona a imagem a tela de Alcool ou gasolina

--- src/screens/04.AlcoolGasolina.js
+++ src/screens/04.AlcoolGasolina.js
@@ -18,7 +18,7 @@ export function AlcoolGasolinaScreen() {
 
   return (
     <View>
-      <Image style={{ width: 200, height: 150 }} source={require('../imagens/alcoolgasolina.png')} />
+      <Image style={{ width: 200, height: 150 }} source={require('../imagens/posto.png')} />
       <TextInput placeholder="Preço do Álcool:" onChangeText={setAlcool} />
       <TextInput placeholder="Preço da Gasolina:" onChangeText={setGasolina} />
       <Button title="Calcular" onPress={calcula} />
