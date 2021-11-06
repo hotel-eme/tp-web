45ec9dcb58f84394a45495445e6fe9e4ce36d6a9
Marcelo Aguiar <aguiar.mcl@gmail.com>
2021-11-05 22:23:48 -0300

    Cria aplicação converter moeda usando API

---
+++ src/screens/19.ConversorMoedasApi.js
@@ -0,0 +1,44 @@
+import axios from 'axios';
+import { Picker } from '@react-native-picker/picker';
+import React, { useState } from 'react';
+import { Button, Text, TextInput, View } from 'react-native';
+
+export function ConversorMoedasApiScreen() {
+  var [valor, setValor] = useState(null);
+  var [moedaOrigem, setMoedaOrigem] = useState(null);
+  var [moedaDestino, setMoedaDestino] = useState(null);
+  var [resultado, setResultado] = useState(null);
+
+  function converterMoeda() {
+    const requisicao = axios.get(`https://economia.awesomeapi.com.br/json/last/${moedaOrigem}-${moedaDestino}`);
+    requisicao.then(function (resposta) {
+      setResultado(resposta.data[`${moedaOrigem}${moedaDestino}`].ask);
+    });
+  }
+
+
+  return (
+    <View>
+      <Text>Conversor de moedas: Dolar, Real e Euro</Text>
+      <Text>Valor:</Text>
+      <TextInput onChangeText={setValor} />
+      <Text>De:</Text>
+      <Picker onValueChange={setMoedaOrigem}>
+        <Picker.Item label="Selecione" />
+        <Picker.Item value="USD" label="Dólar" />
+        <Picker.Item value="EUR" label="Euro" />
+        <Picker.Item value="BRL" label="Real" />
+      </Picker>
+      <Text>Para:</Text>
+      <Picker onValueChange={setMoedaDestino}>
+        <Picker.Item label="Selecione" />
+        <Picker.Item value="USD" label="Dólar" />
+        <Picker.Item value="EUR" label="Euro" />
+        <Picker.Item value="BRL" label="Real" />
+      </Picker>
+      <Button title="Converter" onPress={converterMoeda}></Button>
+      {resultado && <Text>Resultado: {moedaDestino} {resultado}</Text>}
+    </View>
+  );
+}
+
