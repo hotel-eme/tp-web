634658601dd841da7a7f5e718945eb98d2025aee
Evandro Myller <evandro.vieira1@aluno.unip.br>
2021-11-07 01:14:12 -0300

    Exibe tarefas fictícias

--- src/screens/15.Tarefas.js
+++ src/screens/15.Tarefas.js
@@ -1,8 +1,34 @@
-import React from "react"
-import { View } from "react-native"
+import React, { useState } from "react"
+import { Button, Text, TextInput, View } from "react-native"
+
+function Tarefa({ descricao }) {
+  return (
+    <View style={{flex: 1, flexDirection: 'row'}}>
+      <Text style={{flexGrow: 1}}>{descricao}</Text>
+      <Button title="Apagar" />
+    </View>
+  );
+}
 
 export function TarefasScreen() {
+  const [novaTarefa, setNovaTarefa] = useState(null);
+
+  const tarefas = [
+    "Tarefa 1",
+    "Tarefa 2",
+  ];
+
   return (
-    <View></View>
+    <View>
+      <View style={{flex: 1, flexDirection: 'row' }}>
+        <TextInput style={{flexGrow: 1}} placeholder="O que fazer..." onChangeText={setNovaTarefa} />
+        <Button title="+" />
+      </View>
+      <View>
+        {tarefas.map(function (tarefa, i) {
+          return <Tarefa key={i} descricao={tarefa} />
+        })}
+      </View>
+    </View>
   );
 }
