c8632541a800a496385bc9e27883337cf38574f4
Evandro Myller <evandro.vieira1@aluno.unip.br>
2021-11-07 01:57:48 -0300

    Possibilita inclusão de outras APIs no back-end

--- backend/urls.py
+++ backend/urls.py
@@ -1,5 +1,11 @@
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+import alunos.urls
+
+api_router = DefaultRouter()
+api_router.registry.extend(alunos.urls.router.registry)
 
 urlpatterns = [
-    path('api/', include('alunos.urls')),
+    path('api/', include(api_router.urls)),
 ]
