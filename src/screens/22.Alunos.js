d558e5e42be07ac7f00d900659074289b2d06721
Evandro Myller <evandro.vieira1@aluno.unip.br>
2021-11-07 17:23:36 -0300

    Adiciona estrutura de telas na app de alunos

--- src/screens/22.Alunos.js
+++ src/screens/22.Alunos.js
@@ -1,10 +1,51 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
 import { Text, View } from "react-native";
 
-export function AlunosScreen() {
+function ListaAlunos(props) {
   return (
     <View>
-      <Text>alunos</Text>
+      <Text>lista</Text>
     </View>
+  );
+}
+
+function DetalhesAluno(props) {
+  return (
+    <View>
+      <Text>detalhes</Text>
+    </View>
+  );
+}
+
+function CadastroAluno(props) {
+  return (
+    <View>
+      <Text>cadastro</Text>
+    </View>
+  );
+}
+
+function AlteracaoAluno(props) {
+  return (
+    <View>
+      <Text>alteração</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+export function AlunosScreen() {
+  return (
+    <NavigationContainer independent={true}>
+      <Stack.Navigator>
+        <Stack.Screen name="Lista de Alunos" component={ListaAlunos} />
+        <Stack.Screen name="Detalhes do Aluno" component={DetalhesAluno} />
+        <Stack.Screen name="Cadastro de Aluno" component={CadastroAluno} />
+        <Stack.Screen name="Alteração de Aluno" component={AlteracaoAluno} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
