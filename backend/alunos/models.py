67fc6337ce83ae794f9e7cbc44de69f07495f27d
Evandro Myller <evandro.vieira1@aluno.unip.br>
2021-11-10 17:42:24 -0300

    Complementa cadastro do aluno com campos adicionais

--- backend/alunos/models.py
+++ backend/alunos/models.py
@@ -15,6 +15,31 @@ class Aluno(models.Model):
         help_text='O número de CPF do aluno.',
     )
 
+    registro_aluno = models.CharField(
+        max_length=10,
+        verbose_name='registro de aluno',
+        help_text='O número de registro interno do aluno.',
+        null=True, blank=True,  # Campo adicionado na após 1.0.0
+    )
+
+    data_nascimento = models.DateField(
+        verbose_name='data de nascimento',
+        help_text='A data de nascimento do aluno.',
+        null=True, blank=True,  # Campo adicionado na após 1.0.0
+    )
+
+    endereco = models.TextField(
+        verbose_name='endereço',
+        help_text='O endereço do aluno.',
+        null=True, blank=True,  # Campo adicionado na após 1.0.0
+    )
+
+    matriculado = models.BooleanField(
+        verbose_name='matriculado',
+        help_text='Determina se o aluno está matriculado.',
+        default=False,
+    )
+
     class Meta:
         verbose_name = 'aluno'
         verbose_name_plural = 'alunos'
