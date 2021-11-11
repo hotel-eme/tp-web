f2c6ce556801883b27f35f77be2b04fe620d35cb
Evandro Myller <evandro.vieira1@aluno.unip.br>
2021-11-10 21:05:59 -0300

    Configura sistema de navegação

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import './css/index.css';
 
+function App() {
+  return (
+    <section id="app">
+      <h1>Trabalhos Práticos de Front-end</h1>
+      <nav>
+      </nav>
+    </section>
+  );
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+    </Routes>
+  </BrowserRouter>,
   document.getElementById('root')
 );
