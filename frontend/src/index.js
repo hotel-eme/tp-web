0b79da5e04f070f91653aba7594a43c3259f371a
Marcelo Aguiar <aguiar.mcl@gmail.com>
2021-11-11 00:13:05 -0300

    Cria a tela contador de pessoas

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import Container from './common/Container';
 import './css/index.css';
 import MeuPerfilScreen from './screens/01.MeuPerfil';
+import ContadorPessoasScreen from './screens/02.ContadorPessoas';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <h1>Trabalhos Práticos de Front-end</h1>
       <nav>
         <Link to="/01-meu-perfil">01. Meu Perfil</Link>
+        <Link to="/02-contador-pessoas">02. Contador De Pessoas</Link>
       </nav>
     </Container>
   );
@@ -21,6 +23,7 @@ ReactDOM.render(
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/01-meu-perfil" element={<MeuPerfilScreen />} />
+      <Route path="/02-contador-pessoas" element={<ContadorPessoasScreen />} />
     </Routes>
   </BrowserRouter>,
   document.getElementById('root')
