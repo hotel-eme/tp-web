4d73d1b8283085b21bc98e62f5b87370e0cfeabb
Evandro Myller <evandro.vieira1@aluno.unip.br>
2021-11-10 21:48:28 -0300

    Porta a tela de perfil para o front-end web

---
+++ frontend/src/screens/01.MeuPerfil.js
@@ -0,0 +1,19 @@
+import Container from '../common/Container';
+
+export default function MeuPerfilScreen() {
+  return (
+    <Container>
+      <img
+        width="100" height="100"
+        alt="Foto de um gatinho."
+        src="https://placekitten.com/500/500"
+      />
+      <dl>
+        <dt>Dados pessoas:</dt> <dd>John Doe</dd>
+        <dt>Formação:</dt> <dd>ADS</dd>
+        <dt>Experiência:</dt> <dd>Programador</dd>
+        <dt>Projetos:</dt> <dd>Apollo 11</dd>
+      </dl>
+    </Container>
+  );
+}
