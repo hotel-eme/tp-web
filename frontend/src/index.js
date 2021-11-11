b7335fcf0e7ee1ddd6bbbc5939dd4707c42b2d4a
Evandro Myller <evandro.vieira1@aluno.unip.br>
2021-11-10 21:48:28 -0300

    Porta a tela de perfil para o front-end web

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import Container from './common/Container';
 import './css/index.css';
+import MeuPerfilScreen from './screens/01.MeuPerfil';
 
 function App() {
   return (
     <Container>
       <h1>Trabalhos Práticos de Front-end</h1>
       <nav>
+        <Link to="/01-meu-perfil">01. Meu Perfil</Link>
       </nav>
     </Container>
   );
@@ -18,6 +20,7 @@ ReactDOM.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
+      <Route path="/01-meu-perfil" element={<MeuPerfilScreen />} />
     </Routes>
   </BrowserRouter>,
   document.getElementById('root')
